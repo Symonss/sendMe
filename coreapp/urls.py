84d55663437d8316afd0e00274f8eeee3adec8f9
Symonss <somborisymonss@gmail.com>
2019-04-10 15:27:57 +0300

    fixed card

--- coreapp/urls.py
+++ coreapp/urls.py
@@ -5,6 +5,9 @@ from .views import coreapp
 
 urlpatterns = [
     path('', coreapp.home, name='home'),
+    path('login', coreapp.login, name='login'),
+    path('employee/home', coreapp.edb, name='edb'),
+
     # path('create', coreapp.create_order, name='create_order'),
 
 
--- coreapp/views/coreapp.py
+++ coreapp/views/coreapp.py
@@ -7,3 +7,11 @@ from django.utils import timezone
 def home(request):
     jobs = Jobs.objects.filter(deadline__lte=timezone.now()).order_by('deadline')
     return render(request, 'coreapp/home.html', {'jobs':jobs})
+
+def login(request):
+
+    return render(request, 'coreapp/login.html')
+
+def edb(request):
+
+    return render(request, 'coreapp/Employeebd.html')
